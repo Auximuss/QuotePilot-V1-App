88b77e0bfc233ab0b072125767242d25e0591712
Aux <Alexpryer12@outlook.com>
2026-07-14 15:34:59 +0100

    hardcode supabase client

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
+const SUPABASE_URL = "https://mppnrqtfcbapkohsogap.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wcG5ycXRmY2JhcGtvaHNvZ2FwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNzkzMzYsImV4cCI6MjA5ODc1NTMzNn0.QG5fNZyOs03OOyQa03mb067Gg2lAg0EVPD4lDdYyKG0";
+
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  return createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 }
